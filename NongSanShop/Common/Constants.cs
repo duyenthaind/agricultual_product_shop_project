655b8b311e0b438e4cfdfd317fbba9fa6b204553
duyenthaind <duyenthai123456@gmail.com>
2022-01-01 22:14:17 +0700

    feature - check date and update time ok

---
+++ NongSanShop/Common/Constants.cs
@@ -0,0 +1,10 @@
+namespace NongSanShop.Common
+{
+    public class Constants
+    {
+        public static class DateTimePattern
+        {
+            public static readonly string DefaultDateTimePattern = "dd/MM/yyyy";
+        }
+    }
+}
